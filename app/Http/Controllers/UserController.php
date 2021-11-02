d628d3a4b62d8be5923c7a68f649d8992b36dc97
mucahitsndc <mucahit.sndc@gmail.com>
2021-11-02 17:41:52 +0300

    required class added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\CustomController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+
+    public function login(Request $request){
+
+    }
+    public function register(Request $request){
+        $validation = Validator::make($request->all(),[
+            'name' => 'required|min:5,max:25',
+            'surname' => 'required|min:5,max:25',
+            'email' => 'required|email|min:5|max:25',
+        ]);
+        dd($validation->fails());
+    }
+}
